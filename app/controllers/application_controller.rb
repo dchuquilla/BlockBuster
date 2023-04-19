6818e5c4236f37f94764772907cc7c434bcdddd8
dchuquilla <dario.chuquilla@gmail.com>
2023-04-18 22:30:29 -0500

    ✨ Configure devise routes to use JSON as default format
    📝 Configure devise routes to use JSON as default format
    
    📊 List of changes made in bullet points:
    - 🌟 Enhancement: Define application_controler and routes to work with
    json format only
    
    🔗 Related Issues:
    - none
    
    👥 Reviewers:
    - Cabal APP team

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+# root app controller to create early executable methods.
 class ApplicationController < ActionController::API
+  respond_to :json
+  include ActionCOntroller::MimeResponds
 end
